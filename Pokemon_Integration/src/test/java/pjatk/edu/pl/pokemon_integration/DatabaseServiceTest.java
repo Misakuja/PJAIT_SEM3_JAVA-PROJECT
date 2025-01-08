66044e28bc1152120a7bd461c881d2cf60c346a3
misakuja <s30284@pjwstk.edu.pl>
2025-01-08 01:00:30 +0100

    Unit tests in progress for Integration model

--- Pokemon_Integration/src/test/java/pjatk/edu/pl/pokemon_integration/DatabaseServiceTest.java
+++ Pokemon_Integration/src/test/java/pjatk/edu/pl/pokemon_integration/DatabaseServiceTest.java
@@ -6,10 +6,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
+import pjatk.edu.pl.pokemon_data.dto.PokemonDto;
+import pjatk.edu.pl.pokemon_data.dto.ResponseDto.*;
+import pjatk.edu.pl.pokemon_data.entity.Ability;
+import pjatk.edu.pl.pokemon_data.entity.Move;
+import pjatk.edu.pl.pokemon_data.entity.Pokemon;
+import pjatk.edu.pl.pokemon_data.entity.Type;
 import pjatk.edu.pl.pokemon_data.repository.*;
 import pjatk.edu.pl.pokemon_integration.service.DatabaseService;
 
-import static org.mockito.Mockito.mock;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class DatabaseServiceTest {
@@ -47,9 +57,31 @@ public class DatabaseServiceTest {
 
     @Test
     void fetchAndSavePokemonsFetchesDataFromAPIAndSavesToRepository() {
-        //??
+        String url = "https://pokeapi.co/api/v2/pokemon/";
+
+        ResponseDto responseDto = new ResponseDto();
+
+        ResultDto resultDto = new ResultDto();
+        resultDto.setUrl("https://pokeapi.co/api/v2/pokemon/1/");
+        responseDto.setResults(List.of(resultDto));
+        responseDto.setNext(null);
+
+        List<TypeResponseDto> types = List.of(new TypeResponseDto());
+        List<AbilityResponseDto> abilities = List.of(new AbilityResponseDto());
+        List<MoveResponseDto> moves = List.of(new MoveResponseDto());
+
+        PokemonDto pokemonDto = new PokemonDto(1L, 10, "Pikachu", 20, 100, 10, types, abilities, moves);
+
+        when(restTemplate.getForObject(eq(url), eq(ResponseDto.class))).thenReturn(responseDto);
+
+        when(restTemplate.getForObject(eq("https://pokeapi.co/api/v2/pokemon/1/"), eq(PokemonDto.class))).thenReturn(pokemonDto);
+
+        when(typeRepository.findByName(anyString())).thenReturn(Optional.of(new Type()));
+        when(abilityRepository.findByName(anyString())).thenReturn(Optional.of(new Ability()));
+        when(moveRepository.findByName(anyString())).thenReturn(Optional.of(new Move()));
+
+        databaseService.fetchAndSavePokemons();
+
+        verify(pokemonRepository).save(any(Pokemon.class));
     }
-
-
-
 }
